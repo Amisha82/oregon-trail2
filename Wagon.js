bf73d9493210170a278cd4125a5f86a04dac82a4
Amisha <pamisha22@yahoo.com>
2020-07-10 23:58:03 -0400

    oregon trail update1

---
+++ Wagon.js
@@ -0,0 +1,36 @@
+class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.passengers = []
+    }
+    getAvailableSeatCount() {
+        let seatcount = this.capacity - this.passengers.length
+        return seatcount
+    }
+    join(traveler) {
+        if (this.getAvailableSeatCount() > 0) {
+            this.passengers.push(traveler)
+        }
+    }
+    shouldQuarantine() {
+        for (let i = 0; i < this.passengers.length; i++) {
+            if (this.passengers[i].isHealthy === true) {
+                return true
+            }
+            else {
+                return false
+            }
+        }
+    }
+    totalFood() {
+        let totfood = 0
+        if (this.shouldQuarantine() === true) {
+            this.passengers.pop()
+        }
+        for (let j = 0; j < this.passengers.length; j++) {
+            console.log(this.passengers[j].getfood())
+            totfood = totfood + this.passengers[j].getfood()
+        }
+        return totfood
+    }
+}
